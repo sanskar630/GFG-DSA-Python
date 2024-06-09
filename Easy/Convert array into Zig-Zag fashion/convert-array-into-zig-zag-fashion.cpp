8820305d132ececed08caa143a731f8a31b62662
sanskar630 <158493308+sanskar630@users.noreply.github.com>
2024-06-09 20:28:03 +0530

    Updated Solution - GfG to GitHub

--- Easy/Convert array into Zig-Zag fashion/convert-array-into-zig-zag-fashion.cpp
+++ Easy/Convert array into Zig-Zag fashion/convert-array-into-zig-zag-fashion.cpp
@@ -25,16 +25,17 @@ class Array {
 
 class Solution {
   public:
-    void zigZag(int n, vector<int> &arr) {
-        // code here
-        for(int i = 0; i < n - 1; i++){
-            if(i % 2 == 0 && arr[i] > arr[i + 1])
-                swap(arr[i], arr[i + 1]);
-            else if(i % 2 == 1 && arr[i] < arr[i + 1])
-                swap(arr[i], arr[i + 1]);
-        }
+    void zigZag(int n, vector<int> &arr){
+    for(int i=0;i<n-1;i++){
+    if(i%2==0 and arr[i]>arr[i+1])
+        swap(arr[i],arr[i+1]);
+        else if(i%2==1 and arr[i]<arr[i+1])
+        swap(arr[i],arr[i+1]);
     }
+    }
+
 };
+
 
 
 
