fc467f552beae785305ad73befa8b8918695c871
sanskar630 <158493308+sanskar630@users.noreply.github.com>
2024-07-02 00:47:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/linked list of strings forms a palindrome/linked-list-of-strings-forms-a-palindrome.py
@@ -0,0 +1,65 @@
+def compute(head): 
+    #return True/False
+    if(head == None):
+        return True
+    
+    s = ''
+    while(head != None):
+        s += head.data
+        head = head.next
+    
+    n = len(s)
+    for i in range(n//2):
+        if(s[i] != s[n-1-i]):
+            return False
+    return True
+
+
+
+
+ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+#contributed by RavinderSinghPB
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Llist:
+
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data, tail):
+        node = Node(data)
+
+        if not self.head:
+            self.head = node
+            return node
+
+        tail.next = node
+        return node
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for tcs in range(t):
+
+        n1 = int(input())
+        arr1 = input().split()
+        ll1 = Llist()
+        tail = None
+        for nodeData in arr1:
+            tail = ll1.insert(nodeData, tail)
+
+        if compute(ll1.head):
+            print('true')
+        else:
+            print('false')
+
+# } Driver Code Ends
